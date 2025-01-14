8c08d6a8c06ce0485403542a7ca1ddf6f2a6b2f5
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-01-14 21:49:48 +0530

    conditions and loops basics

---
+++ Condition_and_loops/Condition_and_Loops.java
@@ -0,0 +1,25 @@
+package Condition_and_loops;
+
+public class Condition_and_Loops {
+    public static void main(String[] args) {
+ /*       int a=10;
+        if(a==10){
+            System.out.println("True");
+        }else {
+            System.out.println("false");
+        }
+
+        int count=1;
+        while (count<=5){
+            System.out.print(count);
+            count++;
+        }
+        System.out.println();
+        for(int i=1;i!=5;i++){
+            System.out.print(i);
+        }
+
+  */
+
+    }
+}
