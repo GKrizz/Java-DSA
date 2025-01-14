62ee1dd2b3cd328b9362e72350ea9fb96b705408
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-01-14 21:46:57 +0530

    basic data types

---
+++ Primitive.java
@@ -0,0 +1,10 @@
+public class Primitive {
+    public static void main(String[] args) {
+        int num=10;
+        float mark=12.5f;
+        double largeDecimimalNumber=56789.4567;
+        long largeInteger=12547996332445521L;
+        boolean check =false;
+        char letter='a';
+    }
+}
