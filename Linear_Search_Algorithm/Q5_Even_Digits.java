b71c6e0c6e92e901b40fb7de811af251ea378ec0
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-01-25 19:04:21 +0530

    count even count of numbers in array

---
+++ Linear_Search_Algorithm/Q5_Even_Digits.java
@@ -0,0 +1,29 @@
+package Linear_Search_Algorithm;
+
+public class Q5_Even_Digits {
+    static int findNumbers(int[] arr){
+        int count=0;
+        for (int num:arr){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean even(int num){
+        return digits(num)%2==0;
+    }
+
+    static int digits(int num){
+        int count=0;
+        while (num>0){
+            count++;
+            num/=10;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr={345,57,7896};
+        System.out.println(findNumbers(arr));
+    }
+}
