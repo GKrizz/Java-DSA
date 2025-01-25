3588bf60c837dae3e086d3a24840e528ea9238be
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-01-25 18:00:53 +0530

    Min

---
+++ Linear_Search_Algorithm/Q3_Find_minimum.java
@@ -0,0 +1,18 @@
+package Linear_Search_Algorithm;
+
+public class Q3_Find_minimum {
+    static int find_min(int[] arr){
+        int min=arr[0];
+        for(int num:arr){
+            if(num<min){
+                min=num;
+            }
+        }
+        return min;
+
+    }
+    public static void main(String[] args) {
+        int[] arr={3,5,8,1};
+        System.out.println(find_min(arr));
+    }
+}
