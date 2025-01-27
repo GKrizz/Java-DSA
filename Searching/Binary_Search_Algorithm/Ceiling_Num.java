534c948f738a0943d6cd6e260d9788a6d0b1f7c9
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-01-27 20:44:52 +0530

    ceiling num

---
+++ Searching/Binary_Search_Algorithm/Ceiling_Num.java
@@ -0,0 +1,24 @@
+package Searching.Binary_Search_Algorithm;
+
+public class Ceiling_Num {
+    static int search(int[] arr,int target){
+        if(target>arr[arr.length-1]) return -1;
+        int s=0;
+        int e=arr.length-1;
+        while (s<=e){
+            int mid=s+(e-s)/2;
+            if(target>arr[mid])
+                s=mid+1;
+            else if (target<arr[mid])
+                e=mid-1;
+            else
+                return mid;
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,3,5,7};
+        int target=4;
+        System.out.println(search(arr,target));
+    }
+}
