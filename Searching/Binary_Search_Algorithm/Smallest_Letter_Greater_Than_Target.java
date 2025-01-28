f603a83a34415f43e2e104ef952345bc1734e9e7
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-01-28 22:26:16 +0530

    smallest but greater than target

---
+++ Searching/Binary_Search_Algorithm/Smallest_Letter_Greater_Than_Target.java
@@ -0,0 +1,23 @@
+package Searching.Binary_Search_Algorithm;
+
+public class Smallest_Letter_Greater_Than_Target {
+    static char nextGreatestLetter(char[] arr, char target){
+        int s=0;
+        int e=arr.length-1;
+        while (s<=e){
+            int mid=s+(e-s)/2;
+            if(target>=arr[mid])
+                s=mid+1;
+            else
+                e=mid-1;
+        }
+        return arr[s% arr.length];
+
+    }
+    public static void main(String[] args) {
+        char[] arr={'c','f','j'};
+        char target='c';
+        System.out.println(nextGreatestLetter(arr,target));
+
+    }
+}
