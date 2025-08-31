f82a5e71a3640503a6f193b5d720b305094d8b53
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-08-31 20:23:19 +0530

    n natural numbers

---
+++ Recursion/Q3_N_to_1.java
@@ -0,0 +1,12 @@
+package Recursion;
+
+public class Q3_N_to_1 {
+    public static void main(String[] args) {
+        fun(5);
+    }
+    static void fun(int n){
+        if(n==0) return;
+        System.out.print(n + " ");
+        fun(n-1);
+    }
+}
